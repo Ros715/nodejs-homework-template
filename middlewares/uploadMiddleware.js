d5a71ea6c392e372dd173a4bc38651f596242e00
Mariia Ros <rosm0099@gmail.com>
2021-11-22 22:02:24 +0100

    Update uploadMiddleware.js

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,4 +1,3 @@
-// const { BadRequest, Unauthorized } = require("http-errors");
 const multer = require('multer')
 const path = require('path')
 
@@ -19,28 +18,5 @@ const uploadConfig = multer.diskStorage({
 const uploadMiddleware = multer({
   storage: uploadConfig,
 })
-
-/*
-use:
-http://localhost:3000/avatars/5-5_.jpg
-
-const destinationDir = path.join(__dirname,"public/avatar");
-app.patch("/users/avatar",
-  uploadMiddleware.single("file"),
-  try {
-  async (req,res,next) => {
-    console.log(req.file);
-    const {path: tempUpload, originalname} = req.file;
-    const resultUpload = path.join(destinationDir,originalname);
-    await fs.rename(tempUpload,resultUpload);
-    const src = path.join("/public/avatar",originalname);
-    const newProduct = {...req.body, src, id:v4()};
-    res.status(201).json(newProduct);
-  })
-} catch(error) {
-    await fs.unlink(tempUpload);
-}
- where "file" is name of the field KEY
-*/
 
 module.exports = uploadMiddleware
